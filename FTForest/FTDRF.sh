0bc9a4663ef577f091c0a28fbd920230ac39c363
acme <acme@nullspace2.byu.edu>
2017-06-01 11:06:04 -0600

    FTDRF.sh fixed

---
+++ FTForest/FTDRF.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "User note:  This test will use substantial computer memory (>50 GB)."
+echo "If you do not have these memory resources on your machine, we advise you to cancel this task now."
+
+python FTDRF_test.py
